9e39324c00d8fb43c4140e08504a1a6440eda0a7
cifeng1989 <ray.cifeng@gmail.com>
2017-11-08 06:04:38 +0000

    DEFAULT_ZOOM & marker

--- js/nearby_search.js
+++ js/nearby_search.js
@@ -1,15 +1,17 @@
 $(function() {
+    var DEFAULT_ZOOM = 15;
     function initMap() {
-        var position = {lat: 47.6204, lng: -122.3491};
+        var position = {lat: 47.620491, lng: -122.349279};
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 15,
-            center: {
-                lat: 47.6204,
-                lng: -122.3491
-            }
+            zoom: DEFAULT_ZOOM,
+            center: position
+        });
+
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map
         });
     }
-
 
     initMap();
 });
