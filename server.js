cb7859792313171014244956fe43dddf2ed9402d
cifeng1989 <ray.cifeng@gmail.com>
2017-11-08 04:10:47 +0000

    serve hello world

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const PORT = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log('Example app listening on port' + PORT + '!'))
+app.get('/', function(req, res) {
+     res.send('Hello World!');
+});
+
+app.listen(PORT, function() {
+    console.log('Example app listening on port' + PORT + '!'));
+});
