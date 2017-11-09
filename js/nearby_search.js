f3f1e481dd69c0bbe335fdc8555169f2dbab8dd7
cifeng1989 <ray.cifeng@gmail.com>
2017-11-09 04:03:27 +0000

    Get nearby restaurants and show info on click

--- js/nearby_search.js
+++ js/nearby_search.js
@@ -13,28 +13,64 @@ $(function() {
             center: position
         });
 
-        var marker = new google.maps.Marker({
-            position: position,
-            map: map
-        });
+        //$.ajax({
+        //  url: '/places-info',
+        //  data: {
+        //      'type': 'restaurant',
+        //      'lat': position.lat,
+        //      'lng': position.lng,
+        //      'radius': 500
+        //  },
+        //  success: function(data) {
+        //      debugger;
+        //  },
+        //  failure: function(err) {
+        //      debugger;
+        //  }
+        //});
 
-        $.ajax({
-            url: '/places-info',
-            data: {
-                'type': 'restaurant',
-                'lat': position.lat,
-                'lng': position.lng,
-                'radius': 500
-            },
-            success: function(data) {
-                debugger;
-            },
-            failure: function(err) {
-                debugger;
+        var params = {
+            'location': new google.maps.LatLng(position.lat, position.lng),
+            'radius': 500,
+            'type': 'restaurant'
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(params, function(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+               var current_infowindow;
+                _.each(results, function(place) {
+                    var marker = new google.maps.Marker({
+                        position: {
+                            'lat': place.geometry.location.lat(),
+                            'lng': place.geometry.location.lng()
+                        },
+                        map: map,
+                        title: place.name
+                    });
+                    var infowindow_content = '<div id="content">' + 
+                                            '<h1 id="firstHeading" class="firstHeading">' + place.name + '</h1>' +
+                                            '</div>';
+                    var infowindow = new google.maps.InfoWindow({
+                        content: infowindow_content
+                    });
+                    
+                    marker.addListener('click', function() {
+                        if (current_infowindow) {
+                            current_infowindow.close();
+                        }
+                        infowindow.open(map, marker);
+                        current_infowindow = infowindow;
+
+                        showDetailedInfo(place);
+                    });
+                }); 
             }
         });
-
-    }
-
+    
+    };
+    
+    function showDetailedInfor(place) {
+        $('#detailed-info-wrapper').show();
+    };
     initMap();
 });
